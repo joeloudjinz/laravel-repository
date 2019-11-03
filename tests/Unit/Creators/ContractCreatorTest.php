f827eaa438d6c558544b5fe9fb3e6360176b104f
abdellah addoun <addoun.abdellah@gmail.com>
2019-11-03 11:01:23 +0100

    Preparing test creating necessary files & updating composer.json

---
+++ tests/Unit/Creators/ContractCreatorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Inz\Repository\Test\Unit\Creators;
+
+use Orchestra\Testbench\TestCase;
+
+class ContractCreatorTest extends TestCase
+{
+    /** @test */
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
