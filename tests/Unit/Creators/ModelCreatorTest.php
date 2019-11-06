3936077df02b2a3f33306ef447a1195f34c8c48f
abdellah addoun <addoun.abdellah@gmail.com>
2019-11-06 12:02:31 +0100

    Preparing model creator test class

--- tests/Unit/Creators/ModelCreatorTest.php
+++ tests/Unit/Creators/ModelCreatorTest.php
@@ -2,9 +2,28 @@
 
 namespace Inz\Repository\Test\Unit\Creators;
 
+use Inz\Repository\Base\ModelCreator;
 use Orchestra\Testbench\TestCase;
 
 class ContractCreatorTest extends TestCase
 {
-//
+    private $modelName = 'Post';
+    private $modelWithSubDirectory = 'Models/Post';
+
+    /**
+     * Initial values for the attributes of the ModelCreator class.
+     *
+     * @var array
+     */
+    private $attributesData = [
+        'appNamespace' => 'App\\',
+        'antiSlashedInput' => 'Models\Post',
+        'modelName' => 'Post',
+        'modelNamespace' => 'App\Models\Post',
+    ];
+
+    private function createInstance($model = null)
+    {
+        return new ModelCreator($model ?? $this->modelWithSubDirectory);
+    }
 }
