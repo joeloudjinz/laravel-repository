5d739d812cc221c2e7be679c05fc5203dcbb1c97
abdellah addoun <addoun.abdellah@gmail.com>
2019-11-07 10:47:36 +0100

    Creating featur test class for make repository command

---
+++ tests/Feature/MakeRepositoryCommandTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Inz\Repository\Test\Feature;
+
+use Orchestra\Testbench\TestCase;
+use Inz\Repository\Test\Traits\FakeStorageInitiator;
+
+class MakeRepositoryCommandTest extends TestCase
+{
+    use FakeStorageInitiator;
+}
