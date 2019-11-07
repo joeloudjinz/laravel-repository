5b7a68dc437053b19c64df8026ec880f01cb1ad1
abdellah addoun <addoun.abdellah@gmail.com>
2019-11-07 11:11:40 +0100

    Preparing MakeRepositoryCommandTest class

--- tests/Feature/MakeRepositoryCommandTest.php
+++ tests/Feature/MakeRepositoryCommandTest.php
@@ -1,10 +1,14 @@
 <?php
 namespace Inz\Repository\Test\Feature;
 
-use Orchestra\Testbench\TestCase;
 use Inz\Repository\Test\Traits\FakeStorageInitiator;
+use Orchestra\Testbench\TestCase;
 
 class MakeRepositoryCommandTest extends TestCase
 {
     use FakeStorageInitiator;
+
+    private $command = 'make:repository';
+    private $model = 'Post';
+    private $fullModel = 'Blog/Post';
 }
