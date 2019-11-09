6d3bebb08a855df889c15157b6c818fb56b0d43b
abdellah addoun <addoun.abdellah@gmail.com>
2019-11-09 15:17:45 +0100

    Polishing config file comments & some values adapting test suits to changes made in the config file

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,26 @@
 
 namespace Inz\Repository\Test;
 
+use Inz\Base\ConfigurationResolver;
 use Inz\RepositoryServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class TestCase extends OrchestraTestCase
 {
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        config()->set(
+            ConfigurationResolver::$configName . '.base.path',
+            storage_path('framework/testing/disks/app')
+        );
+    }
+
     protected function getPackageProviders($app)
     {
         return [
