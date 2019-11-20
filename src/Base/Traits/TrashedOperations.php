061a9481da4f331120e640aeadcec484d1ab98eb
abdellah addoun <addoun.abdellah@gmail.com>
2019-11-20 17:36:31 +0100

    Update: repository operations implementing methods that handles soft-deleted records

---
+++ src/Base/Traits/TrashedOperations.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Inz\Base\Traits;
+
+/**
+ * Provides a set of methods to deal with trashed records of a certain table, be sure that
+ * you are using soft delete trait in the appropriate model class.
+ */
+trait TrashedOperations
+{
+    /**
+     * Fetches all column's or specific ones data of soft-deleted records.
+     *
+     * @param array $cols
+     *
+     * @return Collection
+     */
+    public function allTrashed($cols = ['*'])
+    {
+        return $this->model->onlyTrashed()->all($cols);
+    }
+    /**
+     * Fetches all column's or specific ones data of both existing & soft-deleted records.
+     *
+     * @param array $cols
+     *
+     * @return Collection
+     */
+    public function allWithTrashed($cols = ['*'])
+    {
+        return $this->model->withTrashed()->all($cols);
+    }
+    /**
+     * Fetches the first trashed row's data.
+     *
+     * @return Model|null
+     */
+    public function firstTrashed()
+    {
+        return $this->model->onlyTrashed()->first();
+    }
+    /**
+     * Fetches a record based on the passed id from soft-deleted records only.
+     *
+     * @param mixed $id
+     *
+     * @return Collection|null
+     */
+    public function findTrashed($id)
+    {
+        return $this->model->onlyTrashed()->find($id);
+    }
+    /**
+     * Fetches a record based on the passed id in all table's records.
+     *
+     * @param mixed $id
+     *
+     * @return Collection|null
+     */
+    public function findWithTrashed($id)
+    {
+        return $this->model->withTrashed()->find($id);
+    }
+}
